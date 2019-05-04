46dbda44505b85a681967324b4e86bd8ec347dd6
davi <davi.wl@gmail.com>
2019-05-04 18:33:04 -0300

    connecting to postgres and adding the models Realtor and Listing

--- projetoImobiliaria/settings.py
+++ projetoImobiliaria/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'projetoImobiliaria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projetoimobiliariadb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost'
     }
 }
 

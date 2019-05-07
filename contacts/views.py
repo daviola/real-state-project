4ffb67c7feca39041a3a53fdd15aeca0dcd7257c
davi <davi.wl@gmail.com>
2019-05-07 17:31:04 -0300

    inquiry verification and email

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -13,6 +14,15 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        #check if usar has made inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
+
         contact = Contact(
             listing=listing,
             listing_id=listing_id,
@@ -23,6 +33,15 @@ def contact(request):
             user_id=user_id)
         
         contact.save()
+
+        # send email
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for' + listing + '. Sign into the admin panel for more info',
+            'email@gmail.com',
+            [realtor_email, 'destination@gmail.com'],
+            fail_silently=False
+        )
 
         messages.success(request, 'Your request has beem submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
